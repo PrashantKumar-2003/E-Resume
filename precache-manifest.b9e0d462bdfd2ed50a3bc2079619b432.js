cdf5da1e909a3d123bc41b9c8c9f25e0ae594f24
Prashant Kumar <97604781+PrashantKumar-2003@users.noreply.github.com>
2022-06-28 16:27:35 +0530

    Updates

---
+++ precache-manifest.b9e0d462bdfd2ed50a3bc2079619b432.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "0eccf1befae804bffc74",
+    "url": "/E-Resume/static/css/main.97e60592.chunk.css"
+  },
+  {
+    "revision": "0eccf1befae804bffc74",
+    "url": "/E-Resume/static/js/main.0eccf1be.chunk.js"
+  },
+  {
+    "revision": "8ee15efa1a2054ecf258",
+    "url": "/E-Resume/static/js/1.8ee15efa.chunk.js"
+  },
+  {
+    "revision": "2d49ffa0346989ef3907",
+    "url": "/E-Resume/static/js/2.2d49ffa0.chunk.js"
+  },
+  {
+    "revision": "a4d7e87c75f876ecc5db",
+    "url": "/E-Resume/static/js/runtime~main.a4d7e87c.js"
+  },
+  {
+    "revision": "3fcba3669e7d86d565a4da5adb92a7f9",
+    "url": "/E-Resume/static/media/Resume Prashant_Kumar.3fcba366.pdf"
+  },
+  {
+    "revision": "fb413ef00b0835aa0c7e4524d19e0803",
+    "url": "/E-Resume/static/media/Owner4.fb413ef0.png"
+  },
+  {
+    "revision": "ca3f543741194a95db63e0bafa75e268",
+    "url": "/E-Resume/static/media/About.ca3f5437.jpg"
+  },
+  {
+    "revision": "93825ffc4de4c3d7df3a377281845fef",
+    "url": "/E-Resume/static/media/instagram.93825ffc.png"
+  },
+  {
+    "revision": "3e803d0953d9ee77db8ba84e05d2b333",
+    "url": "/E-Resume/static/media/gmail.3e803d09.png"
+  },
+  {
+    "revision": "e48b39d612ec8a889e39b5f861a6a922",
+    "url": "/E-Resume/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/E-Resume/precache-manifest.b9e0d462bdfd2ed50a3bc2079619b432.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/E-Resume/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
